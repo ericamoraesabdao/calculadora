987170ed96d7cf6ba42f38540df95607b2594e21
Erica <erica.abdao@zup.com.br>
2024-12-05 10:50:57 -0300

    melhorar calculadora. Adicionar operação módulo e arrumar os if para if else.

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Digite a operação desejada (1. +, 2. -, 3. *, 4. /): ");
+        System.out.print("Digite a operação desejada (1. +, 2. -, 3. *, 4. /, 5. resto): ");
         operacao = Integer.parseInt(entrada.nextLine());
 
         System.out.println("Digite um valor: ");
@@ -22,20 +22,23 @@ public class Main {
             System.out.println("O resultado da adição é: " + (numero1 + numero2));
         }
 
-        if (operacao == 2) {
+        else if (operacao == 2) {
             System.out.println("O resultado da subtração é: " + (numero1 - numero2));
         }
 
-        if (operacao == 3) {
+        else if (operacao == 3) {
             System.out.println("O resultado da multiplicação é: " + (numero1 * numero2));
         }
 
-        if (operacao == 4) {
+        else if (operacao == 4) {
             if (numero2 == 0) {
                 System.out.println("Não podemos fazer divisão por 0");
             } else {
                 System.out.println("O resultado da divisão é: " + (numero1 / numero2));
             }
+        }
+        else if (operacao == 5) {
+            System.out.println("O resto da divisão é: " + (numero1 % numero2));
         }
         else  {
             System.out.println("Operação inválida, Tente novamente!");
